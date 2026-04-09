f46d063a193246625f9ac793bb9573704f268ec7
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-09 07:59:24 +0200

    add completion cmd

---
+++ cmd/cmdCompletion.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2026 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"os"
+
+	"github.com/DanielRivasMD/domovoi"
+	"github.com/DanielRivasMD/horus"
+	"github.com/spf13/cobra"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func CompletionCmd() *cobra.Command {
+	return horus.Must(horus.Must(domovoi.GlobalDocs()).MakeCmd("completion", runCompletion,
+		domovoi.WithArgs(cobra.ExactArgs(1)),
+		domovoi.WithValidArgs([]string{"bash", "zsh", "fish", "powershell"}),
+	))
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func runCompletion(cmd *cobra.Command, args []string) {
+	switch args[0] {
+	case "bash":
+		horus.CheckErr(cmd.Root().GenBashCompletion(os.Stdout))
+	case "zsh":
+		horus.CheckErr(cmd.Root().GenZshCompletion(os.Stdout))
+	case "fish":
+		horus.CheckErr(cmd.Root().GenFishCompletion(os.Stdout, true))
+	case "powershell":
+		horus.CheckErr(cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout))
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
