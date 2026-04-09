18436c9b80788b132feb0d3a22eb14a1d85a6a21
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-09 07:59:09 +0200

    add root

---
+++ cmd/root.go
@@ -0,0 +1,100 @@
+/*
+Copyright © 2026 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"embed"
+	"sync"
+
+	"github.com/DanielRivasMD/domovoi"
+	"github.com/DanielRivasMD/horus"
+	"github.com/spf13/cobra"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//go:embed docs.json
+var docsFS embed.FS
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const (
+	APP     = "zero"
+	VERSION = "v0.1.0"
+	AUTHOR  = "Daniel Rivas"
+	EMAIL   = "<danielrivasmd@gmail.com>"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func InitDocs() {
+	info := domovoi.AppInfo{
+		Name:    APP,
+		Version: VERSION,
+		Author:  AUTHOR,
+		Email:   EMAIL,
+	}
+	domovoi.SetGlobalDocsConfig(docsFS, info)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func GetRootCmd() *cobra.Command {
+	onceRoot.Do(func() {
+		d := horus.Must(domovoi.GlobalDocs())
+		var err error
+		rootCmd, err = d.MakeCmd("root", nil)
+		horus.CheckErr(err)
+
+		rootCmd.PersistentFlags().BoolVarP(&rootFlags.verbose, "verbose", "v", false, "Enable verbose diagnostics")
+		rootCmd.Version = VERSION
+	})
+	return rootCmd
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func Execute() {
+	horus.CheckErr(GetRootCmd().Execute())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type rootFlag struct {
+	verbose bool
+}
+
+var (
+	onceRoot  sync.Once
+	rootCmd   *cobra.Command
+	rootFlags rootFlag
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func BuildCommands() {
+	root := GetRootCmd()
+	root.AddCommand(
+		CompletionCmd(),
+		IdentityCmd(),
+
+	)
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
